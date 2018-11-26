cadc9449eab3ea6aad59ad2814f4cf64ce2ccf50
moory12 <44293438+moory12@users.noreply.github.com>
2018-11-26 16:53:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-the code was written to solve 3 querys 
+### Project: Logs Analysis
+This project is for solving 3 querys 
+ ## What are the most popular three articles of all time?
+ ## Who are the most popular article authors of all time?
+ ## On which days did more than 1% of requests lead to errors?
+ 
+###The database
+ the database called news which contains 3 tables
+ ## articles
+	which have 7 columns (author , title , slug , lead , body , time ,id)
+ ## authors
+	which have 3 columns (name , bio , id)
+ ## log
+	which have 6 columns (path , ip , method , status , time , id)
+
+### Requirements
+	- Vagrant
+	- VirtualBox
+	- the database file 
+	- Python
+	- PostgreSQL
+	- Psycopg2 library
+   
+
 in order to run the code first you need to run vagrant and make sure you have the database file and the test.py file in the vagrant
 then write in the commend python test.py to run the file.
--- test.py
+++ test.py
@@ -1,45 +1,96 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
 import psycopg2
+from datetime import datetime
+
+DBNAME = "news"
 
 
-try:
-	db = psycopg2.connect("dbname=news")		# connecting to the database 
-except psycopg2.Error as e:
-		print "Unable to connect!"
-		print e.pgerror
-		print e.diag.message_detail
-else:
-	print "Connected!"
+def connect(database_name):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except psycopg2.Error as err:
+        print("Unable to connect to database")
+        sys.exit(1)  # The easier method - exit the program
 
-	
-c = db.cursor()
 
-print("What are the most popular three articles of all time?")
+def answer1(cursor):
+    """Answer the first question ..."""
+    query = """  SELECT title,
+                count(*) AS views
+                FROM articles,
+                log
+                WHERE (slug = substring(PATH, 10))
+                GROUP BY articles.title,
+                substring(log.path, 10)
+                ORDER BY views DESC
+                LIMIT 3 ;"""
 
-#first query
-c.execute(" select title,count(*) as views from articles,log where(slug = substring(path,10)) group by articles.title , substring(log.path,10) order by views DESC limit 3  ;");
-result = c.fetchall()
-for i in result:									#printing the result
-	print i[0],str("----"),i[1],str("views")
+    print("Question 1: What are the most popular three articles of all time?")
+    print("\n")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for title, views in result:
+        print("{} -- {} views".format(title, views))
 
-print
 
-print("Who are the most popular article authors of all time?")
-	
-#second query
-c.execute(" select name,count(*) from articles,log,authors where((slug = substring(path,10)) and articles.author = authors.id  ) group by   articles.author,authors.id  order by count(*) DESC limit 4  ;");
-result = c.fetchall()
-for i in result:									#printing the result
-	print i[0],str("----"),i[1],str("views")
-	
-print
+def answer2(cursor):
+    """Answer second question ..."""
+    query = """select name,count(*) from articles,log,authors
+                where((slug = substring(path,10))
+                and articles.author = authors.id)
+                group by articles.author,authors.id
+                order by count(*) DESC limit 4;"""
 
-print("On which days did more than 1% of requests lead to errors?")
-print
+    print("Question 2: Who are the most popular article authors of all time?")
+    print("\n")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for i in result:  # printing the result
+        print
+        i[0], str("----"), i[1], str("views")
 
-#last query
-c.execute(" select date(time),(( cast((select count(date(time)) from log where (log.status = '404 NOT FOUND') group by date(time) order by count(date(time)) DESC limit  1 ) as float)*100 /(select count(date(time)) from log where (log.status = '200 OK') group by date(time) order by count(date(time)) DESC limit 1) ))  as rate from log group by date(time) order by count(date(time)) DESC limit 1; ");
-result = c.fetchall()
-for i in result:									#printing the result
-	print i[0],str("---"),i[1]	
-	
-	
+
+def answer3(cursor):
+    """Answer the third first question ..."""
+    query = """ select date(time),
+                (( cast((select count(date(time))
+                from log where (log.status = '404 NOT FOUND')
+                group by date(time)
+                order by count(date(time)) DESC limit  1 ) as float)*100/
+                (select count(date(time))
+                from log where (log.status = '200 OK')
+                group by date(time)
+                order by count(date(time)) DESC limit 1)))as rate
+                from log
+                group by date(time)
+                order by count(date(time)) DESC limit 1;"""
+
+    print("Question 3: What are the most popular three articles of all time?")
+    print("\n")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for i in result:  # printing the result
+        print
+        i[0], str("---"), i[1]
+
+
+def run():
+    print("Running reporting tools...\n")
+    db, cursor = connect(DBNAME)
+
+    answer1(cursor)
+    print("\n")
+
+    answer2(cursor)
+    print("\n")
+
+    answer3(cursor)
+    print("\n")
+
+
+if __name__ == '__main__':
+    run()
